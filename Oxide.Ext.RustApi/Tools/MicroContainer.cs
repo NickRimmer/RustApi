042bc6050a8837258ef10bd8bcb61ec26564fe12
Nick Rimmer <xan@dipteam.com>
2020-08-06 13:55:45 +0200

    simple dependency injection

---
+++ Oxide.Ext.RustApi/Tools/MicroContainer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Oxide.Ext.RustApi.Tools
+{
+    /// <summary>
+    /// Very small and simple DI container.
+    /// </summary>
+    public class MicroContainer
+    {
+        private delegate object BuilderArgs();
+        private readonly Dictionary<Type, BuilderArgs> _registrations;
+
+        /// <summary>
+        /// Very small and simple DI container.
+        /// </summary>
+        public MicroContainer()
+        {
+            _registrations = new Dictionary<Type, BuilderArgs>();
+        }
+
+        /// <summary>
+        /// Add class with registration by interfaces or inherited class.
+        /// Duplicate will override previous registration.
+        /// </summary>
+        /// <typeparam name="TRegistration">Registration type (e.g. interface).</typeparam>
+        /// <typeparam name="TImplementation">Implementation type.</typeparam>
+        public void Add<TRegistration, TImplementation>() where TImplementation : TRegistration =>
+            _registrations[typeof(TRegistration)] = () => DefaultBuilder(typeof(TImplementation));
+
+        /// <summary>
+        /// Add class to container.
+        /// Duplicate will override previous registration.
+        /// </summary>
+        /// <typeparam name="TRegistration">Registration type.</typeparam>
+        public void Add<TRegistration>() =>
+            _registrations[typeof(TRegistration)] = () => DefaultBuilder(typeof(TRegistration));
+
+        /// <summary>
+        /// Add instance to container.
+        /// Duplicate will override previous registration.
+        /// </summary>
+        /// <typeparam name="TRegistration">Registration type.</typeparam>
+        /// <param name="instance">Instance of registration type.</param>
+        public void Add<TRegistration>(TRegistration instance) =>
+            _registrations[typeof(TRegistration)] = () => instance;
+
+        /// <summary>
+        /// Get instance by registration type.
+        /// </summary>
+        /// <typeparam name="TRegistration">Registration type.</typeparam>
+        /// <returns></returns>
+        /// <exception cref="KeyNotFoundException">Can throw exception in case if requested class wasn't registered.</exception>
+        public TRegistration Get<TRegistration>() => (TRegistration)Get(typeof(TRegistration));
+
+        /// <summary>
+        /// Default builder of instances.
+        /// </summary>
+        /// <param name="implementationType">Type to instantiate.</param>
+        /// <returns></returns>
+        private object DefaultBuilder(Type implementationType)
+        {
+            // let's see what we need to create an object
+            var ctor = implementationType.GetConstructors()[0];
+            var ctorParameters = ctor.GetParameters();
+
+            // try to find all required object in container
+            var parameters = ctorParameters.Select(x => Get(x.ParameterType)).ToArray();
+
+            // create type instance
+            return ctor.Invoke(parameters);
+        }
+
+        /// <summary>
+        /// Looking for class by registration type.
+        /// </summary>
+        /// <param name="registrationType">Registration type.</param>
+        /// <returns></returns>
+        /// <exception cref="KeyNotFoundException">Can throw exception in case if requested class wasn't registered.</exception>
+        private object Get(Type registrationType)
+        {
+            if (!_registrations.TryGetValue(registrationType, out var builder))
+                throw new KeyNotFoundException(registrationType.FullName);
+
+            return builder.Invoke();
+        }
+    }
+}
---
+++ Tests/Oxide.Ext.RustApi.Tests.Unit/MicroContainerTests.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using Oxide.Ext.RustApi.Tools;
+using Xunit;
+
+namespace Oxide.Ext.RustApi.Tests.Unit
+{
+    public class MicroContainerTests
+    {
+        [Fact]
+        public void AddGetTest_ByInterface_Found()
+        {
+            // arrange
+            var container = new MicroContainer();
+
+            // act
+            container.Add<ITestService, TestService>();
+            var result = container.Get<ITestService>();
+
+            // assert
+            Assert.NotNull(result);
+            Assert.Equal(TestService.DefaultValue, result.Value);
+        }
+
+        [Fact]
+        public void AddGetTest_ByInterface_NotFound()
+        {
+            // arrange
+            var container = new MicroContainer();
+
+            // act & assert
+            container.Add<ITestService, TestService>();
+            Assert.Throws<KeyNotFoundException>(() => container.Get<TestService>());
+        }
+
+        [Fact]
+        public void AddGetTest_ByImplementation_Found()
+        {
+            // arrange
+            var container = new MicroContainer();
+
+            // act
+            container.Add<TestService>();
+            var result = container.Get<TestService>();
+
+            // assert
+            Assert.NotNull(result);
+            Assert.Equal(TestService.DefaultValue, result.Value);
+        }
+
+        [Fact]
+        public void AddGetTest_ByImplementation_NotFound()
+        {
+            // arrange
+            var container = new MicroContainer();
+
+            // act & assert
+            container.Add<TestService>();
+            Assert.Throws<KeyNotFoundException>(() => container.Get<ITestService>());
+        }
+
+        [Fact]
+        public void AddGetTest_Instance_Found()
+        {
+            // arrange
+            var container = new MicroContainer();
+            var expectedValue = 100;
+
+            // act
+            container.Add<ITestService>(new TestService { Value = expectedValue });
+            var result = container.Get<ITestService>();
+
+            // assert
+            Assert.NotNull(result);
+            Assert.Equal(expectedValue, result.Value);
+        }
+
+        [Fact]
+        public void AddGetTest_Instance_NotFound()
+        {
+            // arrange
+            var container = new MicroContainer();
+
+            // act & assert
+            container.Add<TestService>(new TestService());
+            Assert.Throws<KeyNotFoundException>(() => container.Get<ITestService>());
+        }
+
+        [Fact]
+        public void InjectionTest_Default_Found()
+        {
+            // arrange
+            var container = new MicroContainer();
+
+            // act
+            container.Add<TestDepService>();
+            container.Add<ITestService, TestService>();
+
+            var result = container.Get<TestDepService>();
+
+            // assert
+            Assert.NotNull(result);
+            Assert.NotNull(result.SomeServiceInstance);
+            Assert.Equal(TestService.DefaultValue, result.SomeServiceInstance.Value);
+        }
+
+        [Fact]
+        public void InjectionTest_Default_NotFound()
+        {
+            // arrange
+            var container = new MicroContainer();
+
+            // act & assert
+            container.Add<TestDepService>();
+
+            var ex = Assert.Throws<KeyNotFoundException>(() => container.Get<TestDepService>());
+            Assert.Equal(typeof(ITestService).FullName, ex.Message);
+        }
+
+        #region Test service interface and implementations
+
+        private interface ITestService
+        {
+            int Value { get; }
+        }
+
+        private class TestService : ITestService
+        {
+            public static int DefaultValue = 10;
+            public int Value { get; set; } = DefaultValue;
+        }
+
+        private class TestDepService
+        {
+            public readonly ITestService SomeServiceInstance;
+
+            public TestDepService(ITestService someService)
+            {
+                SomeServiceInstance = someService ?? throw new ArgumentNullException(nameof(someService));
+            }
+        }
+
+        #endregion
+    }
+}
