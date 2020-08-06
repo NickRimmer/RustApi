84254efc3f2b9635f7187f0997475b161d1c9fb1
Nick Rimmer <xan@dipteam.com>
2020-08-07 01:52:11 +0200

    fix unit test

---
+++ Oxide.Ext.RustApi/RustApiBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using Oxide.Core;
+using Oxide.Core.Extensions;
+using Oxide.Ext.RustApi.Tools;
+
+namespace Oxide.Ext.RustApi
+{
+    public abstract class RustApiBase : Extension
+    {
+        /// <inheritdoc />
+        protected RustApiBase(ExtensionManager manager) : base(manager)
+        {
+
+        }
+
+        /// <inheritdoc />
+        public override string Name => AssemblyInfo.ReadAssemblyAttribute<AssemblyProductAttribute>().Product;
+
+        /// <inheritdoc />
+        public override string Author => AssemblyInfo.ReadAssemblyAttribute<AssemblyCompanyAttribute>().Company;
+
+        /// <inheritdoc />
+        public override VersionNumber Version => AssemblyInfo.ReadAssemblyVersion();
+    }
+}
--- Oxide.Ext.RustApi/RustApiEntry.cs
+++ Oxide.Ext.RustApi/RustApiEntry.cs
@@ -11,7 +11,7 @@ namespace Oxide.Ext.RustApi
     /// <summary>
     /// General entry point for UMod extension.
     /// </summary>
-    public class RustApiEntry : Extension
+    public class RustApiEntry : RustApiBase
     {
         private readonly MicroContainer _services;
         private readonly ILogger<RustApiEntry> _logger;
@@ -22,15 +22,6 @@ namespace Oxide.Ext.RustApi
             _services = BuildServices();
             _logger = _services.Get<ILogger<RustApiEntry>>();
         }
-
-        /// <inheritdoc />
-        public override string Name => AssemblyInfo.ReadAssemblyAttribute<AssemblyProductAttribute>().Product;
-
-        /// <inheritdoc />
-        public override string Author => AssemblyInfo.ReadAssemblyAttribute<AssemblyCompanyAttribute>().Company;
-
-        /// <inheritdoc />
-        public override VersionNumber Version => AssemblyInfo.ReadAssemblyVersion();
 
         /// <inheritdoc />
         public override void OnModLoad()
